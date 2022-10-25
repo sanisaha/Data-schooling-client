4d72ec51a995c80466690d1833d461fd6f472a58
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-10-26 00:41:18 +0300

    navbar added with toggle button

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import './Header.css'
+import logo from '../../../assets/images/data schooling.jpg'
+import { useState } from 'react';
 
 const Header = () => {
+    const [condition, setCondition] = useState('Dark');
+    const handleToggle = () => {
+        if (condition === 'Light') {
+            setCondition('Dark');
+        } else {
+            setCondition('Light');
+        }
+    }
     return (
-        <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <div >
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="dark">
                 <Container>
-                    <Navbar.Brand><Link to='/'>Data-Schooling</Link></Navbar.Brand>
+                    <Image
+                        src={logo}
+                        rounded
+                        style={{ height: '40px', weight: '40px' }}
+                    ></Image>
+                    <Navbar.Brand><Link className='text-decoration-none fs-3 me-6' to='/'>Data-Schooling</Link></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav><Link to='/courses'>Courses</Link></Nav>
-                            <Nav><Link to='/faq'>FAQ</Link></Nav>
-                            <Nav><Link to='/blog'>Blog</Link></Nav>
+                        <Nav className="me-auto fs-4">
+                            <Nav><Link className='text-decoration-none me-4' to='/courses'>Courses</Link></Nav>
+                            <Nav><Link className='text-decoration-none me-4' to='/faq'>FAQ</Link></Nav>
+                            <Nav><Link className='text-decoration-none me-4' to='/blog'>Blog</Link></Nav>
+                            <Nav><Link className='text-decoration-none' onClick={handleToggle}>{condition}</Link></Nav>
 
                         </Nav>
                         <Nav>
